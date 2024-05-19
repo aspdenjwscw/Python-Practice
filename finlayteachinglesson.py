b68adebb049fcc9ce268a1960d5cd45fbe4164e2
aspdenjwscw <aspdenj@wsc.school.nz>
2024-05-20 11:59:53 +1200

    update

--- finlayteachinglesson.py
+++ finlayteachinglesson.py
@@ -3,5 +3,5 @@ import time
 with open("speech.txt", "r") as fp:
     lines = [line.rstrip('\n') for line in fp.readlines()]
     for line in lines:
-        time.sleep(5)
+        time.sleep(0.001)
         print (line)
--- top10.py
+++ top10.py
@@ -1,4 +1,8 @@
-# -------- LISTS -------- 
+# -------- OTHER -------- 
+
+# Guesses
+guesses = []
+play = "yes"
 
 # Top 10
 RICHEST_COUNTRIES_ANSWERS = ["Luxembourg", "Macao SAR", "Ireland", "Singapore", 
@@ -29,6 +33,13 @@ def getLives():
         except:
             print("That wasn't a number.")
 
+# Checks if the answer already exists in lists, checks previous and correct guesses.
+def inList(answer, list):
+    if answer in list:
+        return True
+    else:
+        return False
+
 # Checks if the answer is correct
 def isCorrect(answer, list):
     if answer in list:
@@ -39,20 +50,28 @@ def isCorrect(answer, list):
 
 # -------- MAIN CODE --------
 
-intro()
-lives = getLives()
+intro()   
+while play.lower() == "yes":
+    score = 0
+    lives = getLives()
 
-score = 0
-while lives >= 0:
-    answer = input("Name one of the top 10 richest countries in the world.\nAnswer here: ")
+    while lives >= 1:
+        answer = input("Name one of the top 10 richest countries in the world.\nAnswer here: ")
+        if inList(answer, RICHEST_COUNTRIES_ANSWERS):
+            # checks if guessed already
+            if inList(answer, guesses):
+                print("You've guessed that already.")
+            else:
+                print("Correct!")
+                score += 1
+                guesses.append(answer)
+                print("You have guessed {} of the 10 richest countries. Your score is {}. You have {} chances remaining".format(len(guesses), score, lives))
+        else:
+            print("Wrong!")
+            lives -= 1
+            print("You have guessed {} of the 10 richest countries. Your score is {}. You have {} chances remaining.".format(len(guesses), score, lives))
 
-    if isCorrect(answer, RICHEST_COUNTRIES_ANSWERS):
-        score += 1
-        print("You are correct, good job!")
-        print("1 point has been added to your score, it is now {}".format(score))
-    else:
-        lives -= 1
-        print("You are wrong.")
-        print("1 of your lives has been removed, you now have {} lives.".format(lives))
-
-print("You are out of lives, game over.")
+    # The end
+    print("You are out of lives, game over. You had a final score of {}".format(score))
+        
+    play = input("Would you like to play again? ")
