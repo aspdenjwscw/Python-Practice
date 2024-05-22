920866500ba190fb90b598f1973d7120d04e7310
aspdenjwscw <aspdenj@wsc.school.nz>
2024-05-23 10:06:28 +1200

    removed finlayt teaching lesson

--- finlayteachinglesson.py
+++
@@ -1,7 +0,0 @@
-import time
-
-with open("speech.txt", "r") as fp:
-    lines = [line.rstrip('\n') for line in fp.readlines()]
-    for line in lines:
-        time.sleep(0.001)
-        print (line)
