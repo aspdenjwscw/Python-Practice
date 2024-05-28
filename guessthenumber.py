ca7eea8ea308aa5548c252aa86cedcb720db73e3
aspdenjwscw <aspdenj@wsc.school.nz>
2024-05-28 14:55:35 +1200

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,23 +1,66 @@
-# // Others \\
-import time
 import random
 
-# // Functions \\
+AFFIRMATIONS = ["Alright!", "Cool!", "Sweet!", "Splendid!", "M'kay!", "Sounds good.", "Perfect.", "Okie dokie!"]
 def intro():
-    print("Hello there, this is a guess the number test.")
-    time.sleep(0.5)
+    global name
     name = input("What is your name? ")
-    print("Welcome, {}".format(name))
+    print("Welcome to the number guessing quiz, {}.".format(name))
 
-def question1():
-    question1 = input("Guess a number from 1-5: ")
-    answer1 = random.randint(1-5)
-    if question1 == answer1:
-        print("You are correct!")
+def numguess():
+    global number
+    while True:
+        maxamount = input("What is the maximum number you would like to have? ")
+        try:
+            maxamount = int(maxamount)
+            if maxamount > 1000000:
+                print("Please choose a lower number.")
+            else:
+                print(random.choice(AFFIRMATIONS), "Your maximum number is {}".format(maxamount))
+                break
+        except:
+            print("That wasn't a number.")
+    
+    while True:
+        minamount = input("What is the minimum number you would like to have? ")
+        try:
+            minamount = int(minamount)
+            if minamount > maxamount:
+                print("Please choose a number lower than {}".format(maxamount))
+            else:
+                print(random.choice(AFFIRMATIONS), "Your minimum number is {}".format(minamount))
+                break
+        except:
+            print("That was not a number.")
+    number = random.randint(minamount, maxamount)
+
+def guessing():
+    global number
+    global won
+    global numofguesses
+    while True:    
+        guess = input("What number would you like to guess? ")
+        try:
+            guess = int(guess)
+            break
+        except:
+            print("That wasn't a number.")
+            numofguesses += 1
+    if guess == number:
+        print("You are correct, good job! That took you {} guesses.".format(numofguesses))
+        numofguesses += 1
+        won = True
     else:
-        print("You are correct!")
-        print(answer1)
+        if guess > number:
+            print("Try a lower number.")
+            numofguesses += 1
+        else:
+            print("Try a higher number.")
+            numofguesses += 1
 
-# // Main Code \\
+numofguesses = 0
+won = False
 intro()
-question1()
+numguess()
+guessing()
+while won == False:
+    guessing()
